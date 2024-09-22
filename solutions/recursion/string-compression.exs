234b0b815ad23b594243264a5359a3bf47b24b06
Sameer Kumar <kumar.sam1612@gmail.com>
2024-09-22 18:07:58 +0530

    optimized string compression by chunking on identity

--- solutions/recursion/string-compression.exs
+++ solutions/recursion/string-compression.exs
@@ -2,30 +2,14 @@
 
 defmodule Solution do
   def compress(str) do
-    compress(str, "", 0, String.length(str))
+    str
+    |> String.graphemes()
+    |> Enum.chunk_by(& &1)
+    |> Enum.map(fn ar ->
+      if length(ar) > 1, do: hd(ar) <> Integer.to_string(length(ar)), else: hd(ar)
+    end)
+    |> Enum.join()
   end
-
-  defp compress(str, res, index, str_len) when index < str_len do
-    ch = String.at(str, index)
-    index2 = find_next_diff(str, ch, index + 1, str_len)
-
-    count = index2 - index
-    new_res = res <> ch <> if count == 1, do: "", else: Integer.to_string(count)
-
-    compress(str, new_res, index2, str_len)
-  end
-
-  defp compress(_str, res, _index, _str_len), do: res
-
-  defp find_next_diff(str, ch, index, str_len) when index < str_len do
-    if String.at(str, index) == ch do
-      find_next_diff(str, ch, index + 1, str_len)
-    else
-      index
-    end
-  end
-
-  defp find_next_diff(_str, _ch, index, _str_len), do: index
 
   def run do
     IO.gets("")
